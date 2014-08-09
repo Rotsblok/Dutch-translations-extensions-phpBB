60a5cb931537f5cb86c82f0406ea8910e965226c
Rikkert Westra <rotsblok@phpbbservice.nl>
2014-08-09 16:28:38 +0200

    small change

--- infractions-RC-0.0.1/language/nl/common.php
+++ infractions-RC-0.0.1/language/nl/common.php
@@ -42,33 +42,33 @@ $RULES_URL_TO_REPLACE_INFRACTIONS=$config['phpbb_infractions_rules_url'];
 //
 $lang = array_merge($lang, array(
 	'ACP_DEMO_GOODBYE'					=> 'Zeg maar dag?',
-    'ACP_INF_MANAGER'					=> 'Overtredingen',
-    'ACP_INF_RULES'						=> 'Overtredingregels',
-    'ACP_INF_STATS'						=> 'Overtredingstatistieken',
+	'ACP_INF_MANAGER'					=> 'Overtredingen',
+	'ACP_INF_RULES'						=> 'Overtredingregels',
+	'ACP_INF_STATS'						=> 'Overtredingstatistieken',
 	'ACP_INFRACTION_SAVED'				=> 'Overtreding is succesvol opgeslagen!',
-    'ACP_MANAGE_INFRACTIONS'			=> 'Beheer overtredingen',
-    'ACP_MANAGE_RULES'					=> 'Beheer overtredingregels',
-    'ACP_RULE_MANAGER'					=> 'Overtredingregels',
-    'ACP_RULE_SAVED'					=> 'Regel is succesvol opgeslagen!',
-    'ACP_SAVE_DUPLICATE'				=> 'Dubbel: Regel bestaat al',
-    'ADD_NEW_INFRACTION'				=> 'Nieuwe overtreding toevoegen',
-    'UPDATE_INFRACTIONS_RULES_URL'		=> 'Regels-URL bijgewerkt',
-    'ADD_NEW_RULE'						=> 'Nieuwe regel toevoegen',
-    'ACL_A_INFRACTIONS'					=> 'Kan overtredingen en regels beheren',
-    'ACL_CAT_INFRACTIONS'				=> 'Overtredingen',
-    'ACL_M_INFRACTIONS_ADD'				=> 'Kan boetes uitvaardigen',
-    'ACL_M_INFRACTIONS_REMOVE'			=> 'Kan een bestaande boete verwijderen',
-    'ACL_NO_GROUPS'						=> 'Geen eigen groepen beschikbaar',
-    'MULTIPLE_INFRACTIONS'				=> 'Waarschuwing: Gebruiker heeft al een boete gekregen voor het volgende bericht:',
-    'MISSING_FIELDS'					=> 'Fout:Missende of foutieve data in velden.',
-    'CANNOT_ISSUE_SELF_INFRACTION'		=> 'Cannot issue infraction to self',
-    'EXPIRE_INFRACTIONS'				=> 'Afgelopen boetes verwijderd.',
-    'RECALCULATE_RULE_PENALTIES'		=> 'Herbereken regelboetes',
-    'USER_INFRACTION_ADDED'				=> 'Boete is succesvol uitgevaardigd.',
-    'GO_TO_INFRACTION_LIST'				=> '%s Bekijk lijst van alle overtredingen %s',
-    'LOG_ISSUED_INFRACTION'				=> '<strong>Boete uitgevaardigd aan gebruiker</strong><br />» %s',
-    'LOG_CANCELLED_INFRACTION'			=> '<strong>Boete gebruiker opgeheven</strong>',
-    'INFRACTION_PM_BODY_USER'			=> 'Beste %s, <br />
+	'ACP_MANAGE_INFRACTIONS'			=> 'Beheer overtredingen',
+	'ACP_MANAGE_RULES'					=> 'Beheer overtredingregels',
+	'ACP_RULE_MANAGER'					=> 'Overtredingregels',
+	'ACP_RULE_SAVED'					=> 'Regel is succesvol opgeslagen!',
+	'ACP_SAVE_DUPLICATE'				=> 'Dubbel: Regel bestaat al',
+	'ADD_NEW_INFRACTION'				=> 'Nieuwe overtreding toevoegen',
+	'UPDATE_INFRACTIONS_RULES_URL'		=> 'Regels-URL bijgewerkt',
+	'ADD_NEW_RULE'						=> 'Nieuwe regel toevoegen',
+	'ACL_A_INFRACTIONS'					=> 'Kan overtredingen en regels beheren',
+	'ACL_CAT_INFRACTIONS'				=> 'Overtredingen',
+	'ACL_M_INFRACTIONS_ADD'				=> 'Kan boetes uitvaardigen',
+	'ACL_M_INFRACTIONS_REMOVE'			=> 'Kan een bestaande boete verwijderen',
+	'ACL_NO_GROUPS'						=> 'Geen eigen groepen beschikbaar',
+	'MULTIPLE_INFRACTIONS'				=> 'Waarschuwing: Gebruiker heeft al een boete gekregen voor het volgende bericht:',
+	'MISSING_FIELDS'					=> 'Fout:Missende of foutieve data in velden.',
+	'CANNOT_ISSUE_SELF_INFRACTION'		=> 'Cannot issue infraction to self',
+	'EXPIRE_INFRACTIONS'				=> 'Afgelopen boetes verwijderd.',
+	'RECALCULATE_RULE_PENALTIES'		=> 'Herbereken regelboetes',
+	'USER_INFRACTION_ADDED'				=> 'Boete is succesvol uitgevaardigd.',
+	'GO_TO_INFRACTION_LIST'				=> '%s Bekijk lijst van alle overtredingen %s',
+	'LOG_ISSUED_INFRACTION'				=> '<strong>Boete uitgevaardigd aan gebruiker</strong><br />» %s',
+	'LOG_CANCELLED_INFRACTION'			=> '<strong>Boete gebruiker opgeheven</strong>',
+	'INFRACTION_PM_BODY_USER'			=> 'Beste %s, <br />
 		Je hebt een boete gekregen op  '.$FORUM_NAME_TO_REPLACE_INFRACTIONS.' Forums. <br /><br />
 		<strong>Overtreding:</strong> %s <br />
 		<strong>Reden:</strong> %s <br /><br />
@@ -79,7 +79,7 @@ $lang = array_merge($lang, array(
 		Alleen een beheerder kan een overtreding terugdraaien en het zal alleen gedaan worden als een overtreding foutief is gegeven. Stuur een privébericht naar een <strong>beheerder</strong> als je denkt dat er een fout is opgetreden.<br />
 		Met vriendelijke groeten,<br />
 		'.$FORUM_NAME_TO_REPLACE_INFRACTIONS.' Forums',
-    'INFRACTION_PM_BODY_POST'			=> 'Beste %s, <br />
+	'INFRACTION_PM_BODY_POST'			=> 'Beste %s, <br />
 		Je hebt een boete gekregen op '.$FORUM_NAME_TO_REPLACE_INFRACTIONS.' Forums.<br /><br />
 		<strong>Overtreding:</strong> %s<br />
 		<strong>Reden:</strong> %s</br><br />
@@ -93,5 +93,5 @@ $lang = array_merge($lang, array(
 		<blockquote>%s</blockquote><br />
 		Met vriendelijke groeten,<br />
 		'.$FORUM_NAME_TO_REPLACE_INFRACTIONS.' Forums',
-    'INFRACTION_PM_SUBJECT'			=> 'Je hebt een boete ontvangen op '.$FORUM_NAME_TO_REPLACE_INFRACTIONS.' Forums',
+	'INFRACTION_PM_SUBJECT'			=> 'Je hebt een boete ontvangen op '.$FORUM_NAME_TO_REPLACE_INFRACTIONS.' Forums',
 ));
